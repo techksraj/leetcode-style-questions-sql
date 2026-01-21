609139062138563ddb3a54423e277f80a16e7782
rajk <tech.ksraj541@gmail.com>
2026-01-20 18:22:18 -0800

    Solved few more problems

--- Leetcode_medium_PostgreSQL.sql
+++ Leetcode_medium_PostgreSQL.sql
@@ -29,3 +29,98 @@ AND tiv_2015 IN (
     GROUP BY tiv_2015
     HAVING COUNT(*) > 1
 );
+
+
+
+-------------X----------------X--------------------X-------------------
+
+--https://leetcode.com/problems/friend-requests-ii-who-has-the-most-friends/
+
+--602. Friend Requests II: Who Has the Most Friends
+
+Create table If Not Exists RequestAccepted (requester_id int not null, accepter_id int null, accept_date date null);
+Truncate table RequestAccepted;
+insert into RequestAccepted (requester_id, accepter_id, accept_date) values ('1', '2', '2016/06/03');
+insert into RequestAccepted (requester_id, accepter_id, accept_date) values ('1', '3', '2016/06/08');
+insert into RequestAccepted (requester_id, accepter_id, accept_date) values ('2', '3', '2016/06/08');
+insert into RequestAccepted (requester_id, accepter_id, accept_date) values ('3', '4', '2016/06/09');
+
+ 
+select id, count(*) as num
+from
+(
+select requester_id as id
+from RequestAccepted
+union all
+select accepter_id
+from RequestAccepted as id
+) friends
+group by id
+order by count(*) desc
+limit 1;
+
+
+-------------X----------------X--------------------X-------------------
+
+--https://leetcode.com/problems/tree-node/description/
+
+--608. Tree Node
+
+Create table If Not Exists Tree (id int, p_id int);
+Truncate table Tree;
+insert into Tree (id, p_id) values ('1', NULL);
+insert into Tree (id, p_id) values ('2', '1');
+insert into Tree (id, p_id) values ('3', '1');
+insert into Tree (id, p_id) values ('4', '2');
+insert into Tree (id, p_id) values ('5', '2');
+
+
+SELECT id,
+
+    CASE 
+        WHEN p_id IS NULL THEN 'Root'
+        WHEN id IN (SELECT p_id FROM Tree)THEN 'Inner'
+        ELSE 'Leaf'
+        END AS type
+ FROM Tree;
+
+
+-------------X----------------X--------------------X-------------------
+
+--https://leetcode.com/problems/exchange-seats/description/
+
+--626. Exchange Seats
+
+Create table If Not Exists Seat (id int, student varchar(255));
+Truncate table Seat;
+insert into Seat (id, student) values ('1', 'Abbot');
+insert into Seat (id, student) values ('2', 'Doris');
+insert into Seat (id, student) values ('3', 'Emerson');
+insert into Seat (id, student) values ('4', 'Green');
+insert into Seat (id, student) values ('5', 'Jeames');
+
+
+select id,
+case when id % 2 = 1 and id = max(id) then student
+	 when id % 2 = 0 then (select student from seat1 where id = id - 1)
+	 else (select student from seat1 where id = id + 1)
+from seat;
+
+select max(id) from seat;
+
+
+select 
+case when id % 2 =1 and id+1 in (select id from Seat) then id+1
+     when id % 2 =0 then id-1
+     else id
+     end as id, 
+student
+from Seat
+order by id;
+
+
+
+
+
+
+
