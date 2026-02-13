84b142f7040cb068e1db74a8a1603a784c80b98c
rajuks <tech.ksraj541@gmail.com>
2026-02-13 00:40:49 -0800

    Solved few more problems

--- Leetcode_medium_PostgreSQL.sql
+++ Leetcode_medium_PostgreSQL.sql
@@ -254,13 +254,38 @@ WHERE cum_wei <= 1000
 ORDER BY turn DESC LIMIT 1;
 
 
+-------------X----------------X--------------------X-------------------
 
+--https://leetcode.com/problems/restaurant-growth/description/
 
+--1321. Restaurant Growth
+drop table Customer;
 
+Create table If Not Exists Customer (customer_id int, name varchar(20), visited_on date, amount int);
+Truncate table Customer;
+insert into Customer (customer_id, name, visited_on, amount) values ('1', 'Jhon', '2019-01-01', '100');
+insert into Customer (customer_id, name, visited_on, amount) values ('2', 'Daniel', '2019-01-02', '110');
+insert into Customer (customer_id, name, visited_on, amount) values ('3', 'Jade', '2019-01-03', '120');
+insert into Customer (customer_id, name, visited_on, amount) values ('4', 'Khaled', '2019-01-04', '130');
+insert into Customer (customer_id, name, visited_on, amount) values ('5', 'Winston', '2019-01-05', '110');
+insert into Customer (customer_id, name, visited_on, amount) values ('6', 'Elvis', '2019-01-06', '140');
+insert into Customer (customer_id, name, visited_on, amount) values ('7', 'Anna', '2019-01-07', '150');
+insert into Customer (customer_id, name, visited_on, amount) values ('8', 'Maria', '2019-01-08', '80');
+insert into Customer (customer_id, name, visited_on, amount) values ('9', 'Jaze', '2019-01-09', '110');
+insert into Customer (customer_id, name, visited_on, amount) values ('1', 'Jhon', '2019-01-10', '130');
+insert into Customer (customer_id, name, visited_on, amount) values ('3', 'Jade', '2019-01-10', '150');
 
-
-
-
+with day_sum_table as (
+select visited_on, sum(amount) as day_sum
+from customer
+group by visited_on 
+order by visited_on 
+)
+select visited_on,
+SUM(day_sum) over (rows between 6 PRECEDING and current row) as amount,
+ROUND(AVG(day_sum) over (rows between 6 PRECEDING and current row), 2) as average_amount
+from day_sum_table
+offset 6;
 
 
 
